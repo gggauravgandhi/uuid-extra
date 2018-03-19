9ca356b70ae3154454c3f90be6009290871aa694
Gaurav Gandhi <imgg.gauravgandhi@gmail.com>
2018-03-19 16:13:46 +0530

    Test case title patched
    Signed-off-by: Gaurav Gandhi <imgg.gauravgandhi@gmail.com>

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,7 +2,7 @@
 const invariants = require('invariants');
 const uuidex = require('../index.js');
 
-describe('EX4 function', function(){
+describe('EX14 function', function(){
 	it('It should return 36 char string', function(){
 		invariants(uuidex.ex14().split('').length === 36, "Error: It's not returning 36 char string");
 	});
