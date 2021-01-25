3b10b07cae8f067ca7907503e9016074880a8d05
Gaurav Gandhi <imgg.gauravgandhi@gmail.com>
2021-01-25 13:40:27 +0530

    Update codebase to use modern js & latest uuid repo

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var uuidV1 = require("uuid/v1")
-var uuidV4 = require("uuid/v4")
+const {
+  v1: uuidV1,
+  v4: uuidV4,
+} = require('uuid');
+
 
 module.exports = {
   v1: uuidV1,
   v4: uuidV4,
-  ex14: function ex14() {
-    var v1 = uuidV1().split('');
-    var v4 = uuidV4().split('');
-    var e14 = [];
-    var v1Length = v1.length;
+  ex14: () => {
+    const v1 = uuidV1().split('');
+    const v4 = uuidV4().split('');
+    const e14 = [];
+    const v1Length = v1.length;
 
 
-    for (var i = 0; i < v1Length; i++) {
+    for (let i = 0; i < v1Length; i++) {
       e14.push(Math.round(Math.random()) ? v1[i] : v4[i]);
     }
 
+
     return e14.join('');
-  }
+  },
 };
